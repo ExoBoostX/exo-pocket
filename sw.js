ba9c282087cb5cc43e8b368d046c16d43a71b9b8
ExoBoostX <xdeclxn@gmail.com>
2026-02-16 23:02:23 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "exo-ai-presets-v6";
+const CACHE_NAME = "exo-ai-presets-v7";
 const ASSETS = ["./","./index.html","./style.css","./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
